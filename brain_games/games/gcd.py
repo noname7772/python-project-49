0832d6812478cf83021de7d3f7806ef064b44111
aleksej <alexkgn1@mail.ru>
2023-10-23 22:57:38 +0500

    complete step 7

---
+++ brain_games/games/gcd.py
@@ -0,0 +1,36 @@
+from random import randint, choice
+import prompt
+
+
+def game_rules():
+    print('Find the greatest common divisor of given numbers.')
+
+
+def get_answer():
+    return prompt.string('Your answer: ')
+
+
+def ask_question():
+    num1 = randint(0, 100)
+    num2 = randint(0, 100)
+    print('Question:', f'{num1} {num2}')
+    return num1, num2
+
+
+def get_correct_answer(question):
+    num1, num2 = question
+    return str(is_correct(num1, num2))
+
+
+def is_correct(num1, num2):
+    global gcd
+    if num1 > num2:
+        x = num2
+    else :
+        x = num1
+    for i in range (1, x + 1):
+        if (num1 % i == 0) and (num2 % i == 0):
+            gcd = i
+    return gcd
+
+
---
+++ brain_games/scripts/brain-gcd.py
@@ -0,0 +1,10 @@
+import brain_games.engine as engine
+from brain_games.games import gcd
+
+
+def main():
+    engine.run_game(gcd)
+
+
+if __name__ == '__main__':
+    main()
