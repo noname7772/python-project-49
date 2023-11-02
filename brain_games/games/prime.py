3827d586ff47a1983f78f3b00715ac5b630a5012
aleksej <alexkgn1@mail.ru>
2023-11-02 21:59:40 +0500

    fix build

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -20,6 +20,8 @@ def get_correct_answer(question):
 
 
 def is_prime_number(num):
+    if num <= 1:
+        return False
     for i in range(2, (num//2)+1):
         if num % i == 0:
             return False
